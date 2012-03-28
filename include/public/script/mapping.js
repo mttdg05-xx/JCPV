8e8c921841d0e5839a17637f08e68c7cb44e244d
Magatte <max-05@hotmail.com>
2012-03-28 10:11:02 -0400

    :-)

--- include/public/script/mapping.js
+++ include/public/script/mapping.js
@@ -8,15 +8,15 @@ function Mapping(){
 
 Mapping.prototype.matches = function(metricDef, attrib){}
 
-Mapping.prototype.setAttribute = function(metricDef, attrib, value){}
+Mapping.prototype.setAttribute = function(metricDef, attrib, value, from, to){}
 
 function TooltipDefaultMapping(){
   this.matches = function(metricDef, attrib){
     return attrib === "tooltip";
   }
 
-  this.setAttribute = function(metricDef, attrib, value){
-    set_tooltip(metricDef.metric, value);
+  this.setAttribute = function(metricDef, attrib, value, from, to){
+    set_tooltip(metricDef.metric, value, from, to);
   }
 
 }
@@ -26,11 +26,26 @@ function ColorDefaultMapping(){
     return attrib === "color";
   }
 
-  this.setAttribute = function(metricDef, attrib, value){
-    set_color(metricDef.metric, value);
+  this.setAttribute = function(metricDef, attrib, value, from, to, min, max){
+    set_color(metricDef.metric, value, from, to, min, max);
   }
 
 }
+
+function ColorReferenceMapping(){
+  this.matches = function(metricDef, attrib){
+    return attrib === "color" && metricDef.metric === "reference";
+  }
+
+  this.setAttribute = function(metricDef, attrib, value, from, to, min, max){  
+    set_color(metricDef.metric, 0, from, to, min, max);
+    set_color("first_" + metricDef.metric, 100, from, to, min, max);
+
+
+  }
+
+}
+
 
 
 
@@ -47,13 +62,13 @@ function selectMapping(metricDef, attrib){
 }
 
 
-function set_tooltip(metric, tooltip){
-  $("." + metric).attr('title', tooltip);
+function set_tooltip(metric, tooltip, from, to){
+  $("." + metric + from + "_" + to ).attr('title', tooltip);
 }
 
-function set_color(metric, percent){
-  var rgb = get_color(percent);
-  $("." + metric).css('background-color',  rgb_to_hex(rgb.r, rgb.g, rgb.b));
+function set_color(metric, percent, from, to, min, max){
+  var rgb = get_color(percent, min, max);
+  $("." + metric + from + "_" + to).css('background-color',  rgb_to_hex(rgb.r, rgb.g, rgb.b));
 }
 
 
@@ -90,13 +105,20 @@ function createOptions(metrics_defs){
 
 }
 
-function set_mapping(id, value){
+function set_mapping(id, value, from, to, min, max){
+  //alert("min : " + min + "max : " + max);
+  //alert("^^ " + arguments.length);
   var current_option;
   for(var i = 0; i < options.data.length; i++){
     current_option = options.data[i];
     for(var j = 0; j < current_option.attributes.length; j++){
       if(current_option.attributes[j].id === id){
-        current_option.attributes[j].setAttribute(current_option.metric_def, current_option.attributes[j].name, value);
+        if(arguments.length === 6)
+          current_option.attributes[j].setAttribute(current_option.metric_def, current_option.attributes[j].name, value, from, to, min, max);
+        else 
+          current_option.attributes[j].setAttribute(current_option.metric_def, current_option.attributes[j].name, value, from, to);
+
+
       }
 
     }
@@ -135,19 +157,29 @@ function rgb_to_hex(r, g, b){
 
 }
 
+function hex_to_rgb(hex){
+  
+  return {
+    r : parseInt(hex.substring(0, 2), 16),
+    g : parseInt(hex.substring(2, 4), 16),
+    b : parseInt(hex.substring(4, 6), 16)
+  }
+}
 
-
-// get a color between green |---------------| red
+// get a color between min |---------------| max
 // according to the percent given
-function get_color(percent){
+function get_color(percent, min, max){
   if( 0 > percent || percent > 100 )
     return false;
+  var rgb_min = hex_to_rgb(min);
+  var rgb_max = hex_to_rgb(max);
+  var p_normalized = percent / 100;
 
   return {
 
-    r : Math.round((255 * percent) / 100),
-    g : Math.round((255 * (100 - percent) / 100)),
-    b : 0
+    r : Math.round( rgb_min.r * (1 - p_normalized) + rgb_max.r * p_normalized ),
+    g : Math.round( rgb_min.g * (1 - p_normalized) + rgb_max.g * p_normalized ),
+    b : Math.round( rgb_min.b * (1 - p_normalized) + rgb_max.b * p_normalized )
   };
  
   }
@@ -158,5 +190,6 @@ function get_color(percent){
 
 known_mappings.push(new TooltipDefaultMapping());
 known_mappings.push(new ColorDefaultMapping());
+known_mappings.push(new ColorReferenceMapping());
 
 
